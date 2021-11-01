d4232463b18b2b9d12fdc48d38a64b7b1a607db9
ngechu <ngechu08@gmail.com>
2021-11-01 15:29:06 +0300

    Add app routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CardComponent}from "./components/card/card.component"
 
-const routes: Routes = [];
+const routes: Routes = [
+   {
+    path: '',
+    component: CardComponent,
+    children: [
+      {
+        path: 'movies',
+        component: CardComponent,
+      }
+    ]
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
