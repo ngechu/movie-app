52f12dea366b2a16acf5485ffa2d8400367b7ccf
ngechu <ngechu08@gmail.com>
2021-11-01 21:05:11 +0300

    Make api calls to the server

--- src/app/services/card-api.service.ts
+++ src/app/services/card-api.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {environment} from '../../environments/environment'
 
+
+const baseUrl = environment.apiUrl
 @Injectable({
   providedIn: 'root'
 })
 export class CardApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  //Get top 250 movies
+  getTop250Movies(){
+   return this.http.get(`${baseUrl}/Top250Movies/${environment.apiKey}`)
+  }
+  //Get coming soon
+  getComingSoonMovies(){
+    return this.http.get(`${baseUrl}/ComingSoon/${environment.apiKey}`)
+  }
+  //Get most popular movies
+  getMostPopularMovies(){
+    return this.http.get(`${baseUrl}/MostPopularMovies/${environment.apiKey}`)
+
+  }
+  //Get most popular tv shows
+  getMostPopularTVs(){
+      return this.http.get(`${baseUrl}/MostPopularTvs/${environment.apiKey}`)
+  }
+
+  //Get In Theaters movies
+  getInTheaterMovies(){
+    return this.http.get(`${baseUrl}/InTheaters/${environment.apiKey}`)
+  }
+
+
 }
