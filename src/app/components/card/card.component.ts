16514570e79b8042f77b3f7be89f649f0da0da9e
ngechu <ngechu08@gmail.com>
2021-11-01 21:03:33 +0300

    Fetch categories data on component mount

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {CardApiService} from "../../services/card-api.service"
+import {Top250Movie,ComingSoon,PopularMovie,PopularTvShow,InTheatersMovie} from "../../interfaces/interface"
 
 @Component({
   selector: 'app-card',
@@ -7,9 +9,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
-  constructor() { }
+  top250Movies:Top250Movie[]=[]
+  comingSoonMovies:ComingSoon[]=[]
+  popularMovies:PopularMovie[]=[]
+  popularTvShows:PopularTvShow[]=[]
+  inTheatersMovies:InTheatersMovie[]=[]
+
+  searchText: string=""
+
+  constructor(private cardApiService: CardApiService) {
+
+   }
+
 
   ngOnInit(): void {
+    //Fetch data on mount
+    //Fetch top 250 movies
+     this.cardApiService.getTop250Movies().subscribe((response:any) => {
+      this.top250Movies=response?.items
+    })
+
+    //fetch coming soon
+    this.cardApiService.getComingSoonMovies().subscribe((response:any)=>{
+      this.comingSoonMovies=response?.items
+    })
+
+    //Fetch popular movies
+    this.cardApiService.getMostPopularMovies().subscribe((response:any)=>{
+      this.popularMovies=response?.items
+    })
+    //Fetch popular tv shows
+    this.cardApiService.getMostPopularTVs().subscribe((response:any)=>{
+    this.popularTvShows=response?.items
+    })
+
+    //Fetch In Theaters movies
+    this.cardApiService.getInTheaterMovies().subscribe((response:any)=>{
+      this.inTheatersMovies=response?.items
+    })
+
   }
+
+
+
+
 
 }
