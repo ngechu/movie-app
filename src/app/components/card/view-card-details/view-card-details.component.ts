3d04175828ec40b8bdc391ec2bc6e87cf9c59e5e
ngechu <ngechu08@gmail.com>
2021-11-02 12:08:20 +0300

    Create view card component

---
+++ src/app/components/card/view-card-details/view-card-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-card-details',
+  templateUrl: './view-card-details.component.html',
+  styleUrls: ['./view-card-details.component.scss']
+})
+export class ViewCardDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
