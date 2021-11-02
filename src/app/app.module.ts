530743d773fdf4c99c81e15cb4856fced967e01e
ngechu <ngechu08@gmail.com>
2021-11-02 12:06:35 +0300

    Clean up component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
@@ -13,11 +11,13 @@ import { HeaderComponent } from './components/header/header.component';
 import { CardComponent } from './components/card/card.component';
 // search module
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { ViewCardDetailsComponent } from './components/card/view-card-details/view-card-details.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     CardComponent,
+    ViewCardDetailsComponent,
 
 
   ],
