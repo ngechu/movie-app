6953142b6a484da6ab00da416276c0b70f784cf5
ngechu <ngechu08@gmail.com>
2021-11-04 11:51:44 +0300

    Add new interfaces

--- src/app/interfaces/interface.ts
+++ src/app/interfaces/interface.ts
@@ -1,62 +1,64 @@
-export interface Top250Movie
-    {
-      id: string,
-      rank: string,
-      title: string,
-      fullTitle: string,
-      year: string,
-      image: string,
-      crew: string,
-      imDbRating: string,
-      imDbRatingCount: string
-    }
+export interface Top250Movie {
+  id: string;
+  rank: string;
+  title: string;
+  fullTitle: string;
+  year: string;
+  image: string;
+  crew: string;
+  imDbRating: string;
+  imDbRatingCount: string;
+}
 
+export interface ComingSoon {
+  id: string;
+  title: string;
+  fullTitle: string;
+  year: string;
+  releaseState: string;
+  image: string;
+}
 
-export interface ComingSoon
-    {
-       id: string,
-      title: string,
-      fullTitle: string,
-      year: string,
-      releaseState: string,
-      image: string,
-    }
+export interface PopularMovie {
+  id: string;
+  rank: string;
+  title: string;
+  fullTitle: string;
+  year: string;
+  image: string;
+  crew: string;
+  imDbRating: string;
+  imDbRatingCount: string;
+}
 
-export interface PopularMovie
-       {
-      id: string,
-      rank: string,
-      title: string,
-      fullTitle: string,
-      year: string,
-      image: string,
-      crew: string,
-      imDbRating: string,
-      imDbRatingCount: string
-    }
+export interface PopularTvShow {
+  id: string;
+  rank: string;
+  title: string;
+  fullTitle: string;
+  year: string;
+  image: string;
+  crew: string;
+  imDbRating: string;
+  imDbRatingCount: string;
+}
 
-    export interface PopularTvShow
-       {
-      id: string,
-      rank: string,
-      title: string,
-      fullTitle: string,
-      year: string,
-      image: string,
-      crew: string,
-      imDbRating: string,
-      imDbRatingCount: string
-    }
+export interface InTheatersMovie {
+  id: string;
+  rank: string;
+  title: string;
+  fullTitle: string;
+  year: string;
+  image: string;
+  crew: string;
+  imDbRating: string;
+  imDbRatingCount: string;
+}
 
-    export interface InTheatersMovie
-       {
-      id: string,
-      rank: string,
-      title: string,
-      fullTitle: string,
-      year: string,
-      image: string,
-      crew: string,
-      imDbRating: string,
-      imDbRatingCount: string
-    }
+export interface Movie {
+  id: string;
+  resultType: string;
+  image: string;
+  title: string;
+  description: string;
+}
